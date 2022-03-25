7d8daeefefb3e3c9a5fac83fb200589c621c7db9
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-25 22:31:21 +0300

    add date and days

--- GALLERY/views.py
+++ GALLERY/views.py
@@ -7,7 +7,7 @@ def welcome(request):
 
 import datetime as dt
 #..........
-def news_of_day(request):
+def gallery_of_day(request):
     date = dt.date.today()
     html = f'''
         <html>
@@ -17,3 +17,15 @@ def news_of_day(request):
         </html>
             '''
     return HttpResponse(html)
+    
+
+    def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
