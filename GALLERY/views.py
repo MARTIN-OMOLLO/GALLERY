88d73735eb6fde0a32aa46d39cdb1623edbb5c5e
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-25 22:29:42 +0300

    add response

--- GALLERY/views.py
+++ GALLERY/views.py
@@ -4,3 +4,16 @@ from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to My Gallery')
+
+import datetime as dt
+#..........
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
