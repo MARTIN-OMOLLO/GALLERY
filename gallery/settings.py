f8b7530e23f32cea3b6f7af0812d2821d06456e3
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-28 13:20:35 +0300

    add runtime

--- gallery/settings.py
+++ gallery/settings.py
@@ -1,5 +1,5 @@
 import os
-import django_on-heroku
+import django_heroku
 from decouple import config,Csv
 import dj_database_url
 
@@ -51,7 +51,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Configure Django App for Heroku.
- django_on-heroku.settings(locals())
+ django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
