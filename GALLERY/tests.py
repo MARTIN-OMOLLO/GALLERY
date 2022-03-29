5b4a3fd08b52b2b26b814e08f5bd424eb31fa155
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-29 23:24:08 +0300

    add django

--- GALLERY/tests.py
+++ GALLERY/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
-from .models import Picture,Category
+from .models import Photos,Category
 
 # Create your tests here.
 def test_instance(self):
-        self.assertTrue(isinstance(self.picture, Picture))
+        self.assertTrue(isinstance(self.photo, Photo))
         
 def tearDown(self):
         self.photos.delete_photos()
@@ -15,7 +15,7 @@ def tearDown(self):
 def test_save_method(self):
         self.photo.save_photo()
         photos  = Photos.objects.all()
-        self.assertTrue(len(Picture)>0)
+        self.assertTrue(len(Photos)>0)
 
 def test_search_by_category(self):
         photos = Picture.search_picture_by_category('hope')
