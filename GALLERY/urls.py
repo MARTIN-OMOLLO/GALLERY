b14d033761d94a813a86298de0b40b8af7b43c8f
MARTIN OMOLLO <martin.ochieng@student.moringaschool.com>
2022-03-31 12:39:19 +0300

    add updates

--- GALLERY/urls.py
+++ GALLERY/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
-    path('^details/',views.details,name = 'view_details'),
+    path('^details/',views.details,name = 'view.details'),
     path('^search/', views.search_results, name='search_results'),
 ]
 
